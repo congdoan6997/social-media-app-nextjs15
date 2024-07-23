f6c0f3398c72830c5625a652108a179246f0d259
PhDBui <congdoan6997@gmail.com>
2024-07-23 13:44:05 +0300

    feat: 🎸 session provider
    navbar, session provider with user button

---
+++ src/components/UserButton.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useSession } from "@/app/(main)/SessionProvider";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { logout } from "@/app/(auth)/actions";
+import UserAvatar from "./UserAvatar";
+
+interface UserButtonProps {
+  className?: string;
+}
+
+export default function UserButton({ className }: UserButtonProps) {
+  const { user } = useSession();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className={cn("flex-none select-none rounded-full", className)}>
+          <UserAvatar avatarUrl={user.avatarUrl} size={40} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/users/${user.username}`}>
+          <DropdownMenuItem className="cursor-pointer">
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+          className="cursor-pointer"
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
